fa2590b55363d4db3e3f966b79114472dd07f738
joreg <joreg@vvvv.org>
2025-10-22 09:49:06 +0200

    attempt at DllLoadFix

---
+++ src/Library_DllLoadFix.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace VL.IO.OptiTrack
+{
+    public static class DllLoadFix
+    {
+        public static void LoadNatNetLib()
+        {
+            NativeLibrary.Load("NatNetLib", typeof(DllLoadFix).Assembly, searchPath: default);
+        }
+    }
+}
